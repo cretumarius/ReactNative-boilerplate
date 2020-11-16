1216ff44ff9dd6a96cebfd4b238010ac97dfec24
cretumarius <marius.cretu@cegeka.com>
2020-11-16 17:07:50 +0200

    Added project structure and directory aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _atoms: './src/components/atoms',
+          _molecules: './src/components/molecules',
+          _organisms: './src/components/organisms',
+          _navigation: './src/navigation',
+          _scenes: './src/pages',
+          _services: './src/services',
+          _styles: './src/styles',
+          _utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
